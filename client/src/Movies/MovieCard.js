536a92cc1a9f2b24c71637eae164774dbb35ae51
Tate Keller <tatek1993@gmail.com>
2020-02-27 14:02:00 -0600

    Styled MovieCard.js using styled components and styled app.js with vanilla CSS

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 
 const MovieCardStyle = styled.div `
   width: 70%;
-  background-color: rgba(32, 211, 151, 0.853);
+  background-color: rgba(32, 211, 151, 0.953);
   border-radius: 10px;
   box-shadow: 10px 5px rgba(12, 47, 40, 0.95);
   color: rgb(15, 71, 59);
@@ -17,20 +17,29 @@ const MovieCardStyle = styled.div `
 const HeaderStyle = styled.div `
   text-decoration: none;
   font-weight: bolder;
-  text-shadow: 1px 2px rgba(163, 255, 224, 0.979);
+  text-shadow: 2px 2px 4px rgba(12, 47, 40);
+  color: rgba(163, 255, 224);
   font-size: 1.5rem;
 `
 const H3Style = styled.h3 `
   font-size: 1.5rem;
   text-decoration: none;
   font-weight: bolder;
-  text-shadow: 1px 2px rgba(163, 255, 224, 0.979);
+  text-shadow: 2px 2px 4px rgba(12, 47, 40);
+  color: rgba(163, 255, 224);
   `
 const MovieTitle = styled.h2 `
   font-size: 2rem;
   text-decoration: none;
   font-weight: bolder;
-  text-shadow: 2px 2px rgba(163, 255, 224, 0.979);
+  text-shadow: 2px 2px 4px rgba(12, 47, 40);
+  color: rgba(163, 255, 224);
+`
+const PlainText = styled.p `
+  color: rgb(15, 71, 59);
+  text-decoration: wavy;
+  font-weight: bolder;
+  text-shadow: 1px 1px  rgba(163, 255, 224, 0.979);
 `
 
 const MovieCard = props => {
@@ -39,10 +48,10 @@ const MovieCard = props => {
     <MovieCardStyle className="movie-card">
       <MovieTitle>{title}</MovieTitle>
       <HeaderStyle className="movie-director">
-        Director: <em>{director}</em>
+        Director: <PlainText>{director}</PlainText>
       </HeaderStyle>
       <HeaderStyle className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
+        Metascore: <PlainText>{metascore}</PlainText>
       </HeaderStyle>
       <H3Style>Actors</H3Style>
 
